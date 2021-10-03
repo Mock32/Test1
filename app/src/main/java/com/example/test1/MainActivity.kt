988ad50339ff9ae32f6859ce0c1474c9aff6408e
Yasuhiro Sato <3127218Mock32@users.noreply.github.com>
2021-10-03 09:51:20 +0900

    とりあえず話すことはできた

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.test1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.test1.databinding.ActivityMainBinding //手動で追加
+import android.speech.tts.TextToSpeech  //手動で追加
+import java.util.*
 
 
-
-class MainActivity : AppCompatActivity() {
+//: AppCompatActivityクラスを継承
+//  TextToSpeech.OnInitListenerはインターフェイス
+//      public static interface TextToSpeech.OnInitListener
+//      abstract void	onInit(int status)
+//      CTextToSpeechエンジンの初期化の完了を知らせるために呼び出されます。
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     lateinit var binding: ActivityMainBinding  //手動で追加
+    private var textToSpeech: TextToSpeech? = null // 変数名に？がつくのはNULL許容型
+
+        // ~省略~
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +34,9 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(view)
 
+        //TTS初期化
+        textToSpeech = TextToSpeech(this, this)
+
         var flag = false
         binding.messageTextView.text = "A button"
 
@@ -30,13 +44,44 @@ class MainActivity : AppCompatActivity() {
 
           if(flag){
               binding.messageTextView.text = "Hello"
+              SpeechText("はろー")
               flag = false
 
           }else{
               binding.messageTextView.text = "World"
+              SpeechText("わーるど")
               flag = true
           }
         };
 
     }
+
+    //TTSの初期化後呼ばれる
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            textToSpeech?.let { tts ->
+                val locale = Locale.JAPAN
+                if (tts.isLanguageAvailable(locale) > TextToSpeech.LANG_AVAILABLE) {
+                    tts.language = Locale.JAPAN
+                } else {
+                    // 言語の設定に失敗
+                }
+            }
+
+
+        } else {
+            // Tts init 失敗
+        }
+    }
+
+
+    private fun SpeechText(text:String){
+        textToSpeech?.speak(text,TextToSpeech.QUEUE_FLUSH,null,"ID")
+        }
+
+    override fun onDestroy() {
+        textToSpeech?.shutdown()
+        super.onDestroy()
+    }
+
 }
